74477b02eed80818cbe684769646515c32744511
Nicolas Arevalo <inag.458@gmail.com>
2024-02-10 16:47:06 -0500

    Debbugin to true to see what happened

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://notesapp-three-nu.vercel.app']
+ALLOWED_HOSTS = ['*']
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
